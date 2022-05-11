b515d9680d1101972d156f89f385fddf7655ff6b
Wallyson <wallysom2@gmail.com>
2022-05-11 16:31:52 -0300

    connecting to mongo

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 import express, {json} from 'express';
+import {MongoClient} from 'mongodb';
 import joi from 'joi';
+import dotenv from 'dotenv';
 
 const app = express();
+dotenv.config();
 app.use(json());
+
+let db = null;
+const mongoClient = new MongoClient(process.env.MONGO_URL);
+
+try {
+    await mongoClient.connect();
+    db = mongoClient.db(process.env.DATABASE);
+    console.log('Connected to MongoDB');
+} catch (error) {
+    console.log ('Error connecting to MongoDB:', error);
+}
+
 
 //cadastro
 app.post('/signup', (req, res) => {
@@ -19,12 +34,13 @@ const {name, email, password, confirmPassword} = req.body;
     if(error) {
         return res.status(422).send(error.details.map (detail => detail.message));
     }
-    
-    
+
+   
 });
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
 
