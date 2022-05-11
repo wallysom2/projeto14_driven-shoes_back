0229a40cbf79465c7ba9f6476c4b6159bb0a6596
Wallyson <wallysom2@gmail.com>
2022-05-11 14:03:55 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
